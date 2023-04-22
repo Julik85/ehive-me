21404fdc2b0645b808360836dbfdc42f153b8376
Julik85 <33938689+Julik85@users.noreply.github.com>
2023-04-22 11:25:50 +0000

    Deploying to gh-pages from @ Julik85/ehive-me@ea128cd5b0bfea163fd31c23e66e7384a6cde70c 🚀

--- index.cd7b50f6.js
+++
@@ -1,2 +0,0 @@
-$(".autoplay-why-us").slick({dots:!0,slidesToShow:1,slidesToScroll:1,arrows:!1,autoplay:!0,autoplaySpeed:5e3}),$(".autoplay-gallery").slick({slidesToShow:1,slidesToScroll:1,fade:!0,arrows:!1,autoplay:!0,autoplaySpeed:6e3});
-//# sourceMappingURL=index.cd7b50f6.js.map
